ba8b8de7ca2c6ce4cea6a50e12abe2c602569ce6
Treemcio <67553502+Treemcio@users.noreply.github.com>
2020-09-09 15:44:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let chalk = require("chalk")
+
+
+
+
+module.exports = {
+ info: chalk.bold.blue('ℹ'),
+	success: chalk.bold.green('✔'),
+	warning: chalk.bold.yellow('⚠'),
+	error: chalk.bold.red('✖'),
+	ask: chalk.bold.magenta("¿?"),
+	church: chalk.grey.bold('✝'),
+	music: chalk.bold.blue('♪'),
+	right: chalk.bold.white('↑'),
+	left: chalk.bold.white('←'),
+	down: chalk.bold.white('↓'),
+	up: chalk.bold.white('↑'),
+	infinity: chalk.bold.white('∞'),
+	left_right: chalk.bold.white('↔'),
+	up_down: chalk.bold.white('↨'),
+	full_block: chalk.bold('█'),
+	full_block2: chalk.bold("▀"),
+	full_block3: chalk.bold("▐"),
+	full_block4: chalk.bold("▌"),
+	full_block5: chalk.bold("▄"),
+	full_block6: chalk.bold("▬"),
+	empty_block: chalk.bold("░"),
+        flag: chalk.bold("⚐"),
+	dead: chalk.bold("☠"),
+	star: chalk.bold("☆")
+}
+	
+	
